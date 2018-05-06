839baf46d5ab72404a02fffd389f08683546c2ba
Tim Stevens <timstevens@hush.com>
2018-05-05 23:32:06 -0500

    refactored my code

--- liri.js
+++ liri.js
@@ -7,35 +7,28 @@ var client = new Twitter(myKeys.twitter);
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(myKeys.spotify);
 
-var liriCommand;
-var myTitle;
 argvHandler();
 
 function argvHandler() {
-    liriCommand = process.argv[2];
+    var liriCommand = process.argv[2];
+    var myTitle;
     if (process.argv[3]) {
         myTitle = process.argv.slice(3).join(' ');
-    } else {
-        if (liriCommand === 'spotify-this-song') {
-            myTitle = 'The Sign, Ace of Base';
-        } else if (liriCommand === 'movie-this') {
-            myTitle = 'Mr. Nobody';
-        } 
     };
     switch (liriCommand) {
         case "my-tweets":
             myTweets();
             break;
         case "spotify-this-song":
-            mySong();
+            mySong(myTitle);
             break;
         case 'movie-this':
-            myMovie();
+            myMovie(myTitle);
             break;
         case 'do-what-it-says':
             doWhatItSays();
             break;
-    }
+    };
 };
 
 function myTweets() {
@@ -53,7 +46,7 @@ function myTweets() {
     })
 };
 
-function mySong() {
+function mySong(myTitle = 'The Sign, Ace of Base') {
     spotify.search({type: 'track', query: myTitle, limit: '2'})
     .then(function(response) {
         console.log('Song: ' + response.tracks.items[0].name + '\nArtist: ' + response.tracks.items[0].artists[0].name + '\nAlbum: ' + response.tracks.items[0].album.name + '\nPreview: ' + response.tracks.items[0].preview_url + '\nFull Song: ' + response.tracks.items[0].external_urls.spotify);
@@ -63,23 +56,22 @@ function mySong() {
     })
 };
 
-function myMovie() {
+function myMovie(myTitle = 'Mr. Nobody') {
     var baseUrl = 'http://www.omdbapi.com/?';
     var omdbKey = 'apikey=trilogy';
     var queryUrl = baseUrl + omdbKey + '&t=' + myTitle;
     request(queryUrl, function (error, response, body) {
-        console.log(queryUrl);
         if (error) {
             console.log(error);
             console.log('statusCode:', response && response.statusCode);
         }
-        else {  // Handled a movie that does not have rotten tomatoes rating
+        else {
             var myObject = JSON.parse(body);
             if (myObject.Ratings && myObject.Ratings[1] && myObject.Ratings[1].Source === 'Rotten Tomatoes') {
                 var rottenTomatoes = myObject.Ratings[1].Value;
                 console.log('Title: ' + myObject.Title + '\nYear: ' + myObject.Year + '\nIMDB Rating: ' + myObject.imdbRating + '\nRotten Tomatoes Rating: ' + rottenTomatoes + '\nCountry: ' + myObject.Country + '\nLanguage: ' + myObject.Language + '\nPlot: ' + myObject.Plot + '\nActors: ' + myObject.Actors);
-            } 
-            else {
+            }
+            else {  // Handle a movie that does not have rotten tomatoes rating
                 console.log('Title: ' + myObject.Title + '\nYear: ' + myObject.Year + '\nIMDB Rating: ' + myObject.imdbRating + '\nCountry: ' + myObject.Country + '\nLanguage: ' + myObject.Language + '\nPlot: ' + myObject.Plot + '\nActors: ' + myObject.Actors);
             }
         }
@@ -94,15 +86,7 @@ function doWhatItSays() {
         var array = data.split(',');
         var liriCommand = array[0];
         var myTitle = array[1].slice(1,-1);
-
-        spotify.search({type: 'track', query: myTitle, limit: '2'})
-        .then(function(response) {
-            console.log('Song: ' + response.tracks.items[0].name + '\nArtist: ' + response.tracks.items[0].artists[0].name + '\nAlbum: ' + response.tracks.items[0].album.name + '\nPreview: ' + response.tracks.items[0].preview_url + '\nFull Song: ' + response.tracks.items[0].external_urls.spotify);
-        })
-        .catch(function(err) {
-            console.log(err)
-        })
-
+        mySong(myTitle);
     })
 };
 
